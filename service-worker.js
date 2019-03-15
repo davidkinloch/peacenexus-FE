df3df94b8cb5e901f4f355e864002c66b58bf137
David Kinloch <dkinloch@gmail.com>
2019-03-15 16:26:13 +0100

    service worker install

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(dataCacheName).then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
