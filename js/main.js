9124cb3b5ec1005c5fe2a1301b339089f86c5575
David Kinloch <dkinloch@gmail.com>
2019-03-15 15:48:54 +0100

    Added service worker

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@
     $('.sidenav').sidenav();
 
   }); // end of document ready
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 })(jQuery); // end of jQuery name space
---
+++ service-worker.js
@@ -0,0 +1,48 @@
+
+    
+var dataCacheName = 'peacenexus';
+var cacheName = 'peacenexus';
+var filesToCache = [
+  '/',
+ "./index.php",
+ "./manifest.json",
+ "./js/build/production.min.js",
+ "./js/vendor/jquery-2.1.1.min.js",
+ "./service-worker.js",
+ "./css/materialize.css"
+];
+
+// Listen to installation event
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(dataCacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
