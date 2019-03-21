67376d1f9b971add1a093ba30a4861f90b796bb2
David Kinloch <dkinloch@gmail.com>
2019-03-21 10:21:33 +0100

    propsal heade rtext full width for mobile

--- proposals.php
+++ proposals.php
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col s6">
+      <div class="col s12 m6">
         <h1>Calls for Proposals</h1>
         <h2>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia consectetur adipisicing elit</h2>
         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
